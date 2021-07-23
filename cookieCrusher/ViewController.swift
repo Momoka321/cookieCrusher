711d280146d26218cd2e43d4c2124dca5439fbbd
Momoka321 <s1810377@edu.cc.uec.ac.jp>
2021-07-23 19:52:19 +0900

    カウント機能(仮) 画像切り替えはこれからです

--- cookieCrusher/ViewController.swift
+++ cookieCrusher/ViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var countLabel: UILabel!
-    var count:Int = 100
+    @IBOutlet weak var cookieButton: UIButton!
+    var count:Int = 40
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         countLabel.text = String(count)
+        self.cookieButton.imageView?.contentMode = .scaleAspectFit
+        self.cookieButton.contentHorizontalAlignment = .fill
+        self.cookieButton.contentVerticalAlignment = .fill
+        self.cookieButton.imageEdgeInsets = UIEdgeInsets(top: 400, left: 400, bottom: 400, right: 400)
+        let cookieImage = UIImage(named: "cookie1")
+        self.cookieButton.setImage(cookieImage, for: .normal)
     }
 
     @IBAction func cookieButton(_ sender: Any) {
         count -= 1
         countLabel.text = String(count)
+        if (count == 0) {
+            cookieButton.isEnabled = false
+        }
     }
     
 }
