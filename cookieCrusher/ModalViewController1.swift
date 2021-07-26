3aa631b869e465b615a77f2708da079f86820176
katoshinyu <shinyu333@gmail.com>
2021-07-27 01:01:18 +0900

    遷移のための４つのViewController追加, それぞれ全画面・スタート画面に遷移可能, ViewControllerのコードを少しいじりました

---
+++ cookieCrusher/ModalViewController1.swift
@@ -0,0 +1,30 @@
+//
+//  ModalViewController1.swift
+//  cookieCrusher
+//
+//  Created by 加藤慎融 on 2021/07/26.
+//  Copyright © 2021 kashiharaLab. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
