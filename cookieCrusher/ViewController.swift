30db7922331584fec94123b840c80f10072e8fe9
Momoka321 <s1810377@edu.cc.uec.ac.jp>
2021-07-27 19:09:06 +0900

    メインの背景追加

--- cookieCrusher/ViewController.swift
+++ cookieCrusher/ViewController.swift
@@ -16,6 +16,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignBackground()
+    }
+    
+    func assignBackground() {
+        let background = UIImage(named: "background")
+        var imageView : UIImageView
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
     }
     
     override func viewDidLayoutSubviews() {
